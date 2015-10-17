56b92a4a5876201b502a26aa5fe104be90d1ae77
zhanghanyuan <zhanghanyuan@ZhangHanyuandeMacBook-Pro.local>
2015-10-17 23:37:22 +0800

    add register and login function

--- MatchCards/app/src/main/java/com/fdusoft/matchcards/LoginActivity.java
+++ MatchCards/app/src/main/java/com/fdusoft/matchcards/LoginActivity.java
@@ -2,13 +2,25 @@ package com.fdusoft.matchcards;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.database.Cursor;
+import android.app.AlertDialog;
+import android.database.sqlite.SQLiteException;
 
 public class LoginActivity extends Activity {
+
+	private SQLiteDatabase db;
+
+
+	protected void onDestroy() {
+		super.onDestroy();
+		db.close();
+	}
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +33,13 @@ public class LoginActivity extends Activity {
         final EditText passwordText = (EditText) findViewById(R.id.password);
         Button loginButton = (Button) findViewById(R.id.login_button);
         Button registerButton = (Button) findViewById(R.id.register_button);
-        
+
+		db = SQLiteDatabase.openOrCreateDatabase(LoginActivity.this.getFilesDir().toString()
+				+ "/test.dbs", null);
+
         //Set listener
         loginButton.setOnClickListener(new View.OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				String username = usernameText.getText().toString();
@@ -39,19 +54,25 @@ public class LoginActivity extends Activity {
 					} else {
 						// TODO: add login check
 						// If username and password correct, switch to MainActivity
-						Intent intent = new Intent();
-						// Package user info and pass to MainActivity
-						Bundle bundle = new Bundle();
-						bundle.putString("USER_NAME", username);
-						intent.putExtras(bundle);
-						// Switch activity
-						intent.setClass(LoginActivity.this, MainActivity.class);
-						LoginActivity.this.startActivity(intent);
-						LoginActivity.this.finish();
+						if (isUserinfo(username, password) == true) {
+							Intent intent = new Intent();
+							// Package user info and pass to MainActivity
+							Bundle bundle = new Bundle();
+							bundle.putString("USER_NAME", username);
+							intent.putExtras(bundle);
+							// Switch activity
+							intent.setClass(LoginActivity.this, MainActivity.class);
+							LoginActivity.this.startActivity(intent);
+							LoginActivity.this.finish();
+						} else {
+							Toast.makeText(getApplicationContext(), "账号密码错误!",
+									Toast.LENGTH_SHORT).show();
+						}
 					}
 				}
 			}
 		});
+
         registerButton.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
@@ -63,5 +84,27 @@ public class LoginActivity extends Activity {
 		});
         
     }
-    
+
+	public Boolean isUserinfo(String name, String pwd) {
+		try{
+			String str="select * from tb_user where name=? and password=?";
+			Cursor cursor = db.rawQuery(str, new String []{name,pwd});
+			if(cursor.getCount()<=0){
+				new AlertDialog.Builder(LoginActivity.this).setTitle("错误")
+						.setMessage("帐号或密码错误！").setPositiveButton("确定", null)
+						.show();
+				return false;
+			}else{
+				new AlertDialog.Builder(LoginActivity.this).setTitle("正确")
+						.setMessage("成功登录").setPositiveButton("确定", null)
+						.show();
+				return true;
+			}
+
+		}catch(SQLiteException e){
+		}
+		return false;
+	}
+
 }
+
