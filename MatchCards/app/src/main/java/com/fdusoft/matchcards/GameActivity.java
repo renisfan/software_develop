74994ed3d96a282005681edf80f86fdbe6d94cec
Xiaoye Guo <sd1f4140@126.com>
2015-10-19 18:49:02 +0800

    Fix UI & Game bugs
    Fix UI for login/register.
    Fix game bugs, improve game level & UI design.
    Move logout check to MainActivity.finish()
    Code cleanup & add comments.

--- MatchCards/app/src/main/java/com/fdusoft/matchcards/GameActivity.java
+++ MatchCards/app/src/main/java/com/fdusoft/matchcards/GameActivity.java
@@ -1,9 +1,5 @@
 package com.fdusoft.matchcards;
 
-import java.util.Random;
-import java.util.Timer;
-import java.util.TimerTask;
-
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
@@ -18,15 +14,23 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.GridView;
 import android.widget.TextView;
 
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class GameActivity extends Activity {
 
-    private static final int[] levelSize = {5, 5, 6, 6, 7, 7};
-    private static final int[] levelTime = {45, 30, 60, 45, 90, 60};
+    private static final int[] levelCol  = { 4,  5,  5,  5,  5,  6,  6,  6,  6};
+    private static final int[] levelSize = {12, 20, 20, 30, 30, 42, 42, 42, 42};
+    private static final int[] levelTime = {45, 60, 45, 90, 60, 90, 60, 45, 30};
 
     private static Handler handler;
+    private static final int EVENT_CLOSE_CARD = 0;
+    private static final int EVENT_TIME_UP = 1;
 
 	private int cardCount;
-	private int level = 1;
+	private int level = 0;
+    private int displayLevel = 1;
 	private int score = 0;
 	private GridView gridView;
 	private CardAdapter cardAdapter;
@@ -34,6 +38,7 @@ public class GameActivity extends Activity {
 	private TextView levelText;
 	
 	private int[] pattern = new int[50];
+    private static final int TOTAL_PATTERN = 10;
 	
 	private int[] card = new int[50];
 	private int available;
@@ -58,18 +63,19 @@ public class GameActivity extends Activity {
         getWindowManager().getDefaultDisplay().getMetrics(dMetrics);
         width = dMetrics.widthPixels;
 
+        // NOTE: Android Studio gives wrong warning about handler not being static, just ignore.
         handler = new Handler() {
             @Override
             public void handleMessage(Message msg) {
                 switch (msg.what) {
-                    case 0:
+                    case EVENT_CLOSE_CARD:
                         firstCard.setState(0);
                         secondCard.setState(0);
                         firstCard = null;
                         secondCard = null;
                         cardAdapter.notifyDataSetChanged();
                         break;
-                    case 1:
+                    case EVENT_TIME_UP:
                         new AlertDialog.Builder(GameActivity.this).setTitle("时间到！")
                                 .setMessage(String.format("您的分数:%d", score))
                                 .setPositiveButton("确定", new DialogInterface.OnClickListener() {
@@ -84,7 +90,8 @@ public class GameActivity extends Activity {
                                         // TODO: implement share
                                         waiting=false;
                                     }
-                                }).show();
+                                })
+                                .setCancelable(false).show();
                         break;
                 }
                 super.handleMessage(msg);
@@ -101,46 +108,71 @@ public class GameActivity extends Activity {
 
     }
 
+    @Override
+    public void onBackPressed()
+    {
+        new AlertDialog.Builder(GameActivity.this).setTitle("确定要放弃吗？")
+                .setMessage("您的成绩将不会被保存")
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        timer.cancel();
+                        GameActivity.this.setResult(RESULT_CANCELED);
+                        GameActivity.super.onBackPressed();
+                    }
+                })
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                }).show();
+    }
+
     private void startGame() {
 
         scoreText.setText(String.format(getString(R.string.game_score), score));
-        levelText.setText(String.format(getString(R.string.game_level), level, levelTime[level - 1]));
+        levelText.setText(String.format(getString(R.string.game_level), displayLevel, levelTime[level]));
 
-        cardAdapter = new CardAdapter(getApplicationContext(), levelSize[level-1], width);
+        cardAdapter = new CardAdapter(getApplicationContext(), levelCol[level], levelSize[level], width);
 
         cardCount = cardAdapter.getCount();
 
+        // Generate random game pool
         initializeCardPool();
         for (int i=1; i<=cardCount/2; ++i) {
-            pattern[takeRandomCard()]=(i-1)%10+1;
-            pattern[takeRandomCard()]=(i-1)%10+1;
+            pattern[takeRandomCard()]=(i-1)%TOTAL_PATTERN+1;
+            pattern[takeRandomCard()]=(i-1)%TOTAL_PATTERN+1;
         }
         left = cardCount;
 
-        gridView.setNumColumns(levelSize[level - 1]);
+        gridView.setNumColumns(levelCol[level]);
         gridView.setAdapter(cardAdapter);
         gridView.setOnItemClickListener(new OnItemClickListener() {
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
-                // TODO Auto-generated method stub
+                // Do nothing if the last pair of mismatched cards aren't closed yet
                 if (secondCard != null) {
                     return;
                 }
                 CardItem item = (CardItem) cardAdapter.getItem(position);
                 if (!item.isOpen()) {
+                    // Open the card
                     item.setState(pattern[position]);
                     if (firstCard == null) {
                         firstCard = item;
                     } else {
                         secondCard = item;
                         if (firstCard.getState() == secondCard.getState()) {
+                            // Add score if the 2 cards matches, leave them open.
                             score += 10;
                             scoreText.setText(String.format(getString(R.string.game_score),
                                     score));
                             firstCard = null;
                             secondCard = null;
+                            // Move to next level if all cards are matched
                             left -= 2;
                             if (left == 0) {
                                 timer.cancel();
@@ -149,24 +181,27 @@ public class GameActivity extends Activity {
                                         .setPositiveButton("进入下一关", new DialogInterface.OnClickListener() {
                                             @Override
                                             public void onClick(DialogInterface dialog, int which) {
-                                                ++level;
+                                                ++displayLevel;
+                                                level = Math.min(level+1, levelSize.length-1);
                                                 GameActivity.this.startGame();
                                             }
                                         }).show();
                             }
                         } else {
+                            // Close mismatched cards after a while
                             Timer closeTimer = new Timer();
                             closeTimer.schedule(new TimerTask() {
                                 @Override
                                 public void run() {
                                     Message message = new Message();
-                                    message.what = 0;
+                                    message.what = EVENT_CLOSE_CARD;
                                     handler.sendMessage(message);
                                 }
                             }, 500);
                         }
                     }
                 }
+                // Refresh grid view
                 cardAdapter.notifyDataSetChanged();
             }
 
@@ -178,18 +213,21 @@ public class GameActivity extends Activity {
             @Override
             public void run() {
                 waiting = true;
+                // Call handler to open result confirmation dialog
                 Message message = new Message();
-                message.what = 1;
+                message.what = EVENT_TIME_UP;
                 handler.sendMessage(message);
+                // Wait for the dialog to close
                 while (waiting) {}
+                // Send the result back and finish
                 Intent intent = new Intent();
                 Bundle data = new Bundle();
                 data.putInt("GAME_SCORE", score);
                 intent.putExtras(data);
-                GameActivity.this.setResult(0, intent);
+                GameActivity.this.setResult(RESULT_OK, intent);
                 GameActivity.this.finish();
             }
-        }, levelTime[level-1] * 1000);
+        }, levelTime[level] * 1000);
 
     }
     
